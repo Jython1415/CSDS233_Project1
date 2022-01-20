7fc7bd38de40a1a70b21c983d38c5e3dd3d0743b
Joshua Shew <joshua.t.shew@gmail.com>
2022-01-20 16:23:36 -0500

    Method headers for NumList

--- src/NumArrayList.java
+++ src/NumArrayList.java
@@ -4,6 +4,27 @@ public class NumArrayList implements NumList {
         return true;
     }
 
+    public double lookup(int i) {
+        return 0.0;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof NumArrayList) {
+            NumArrayList l = (NumArrayList)o
+            for (int i = 0; i < this.size(); i++) {
+                if (i == l.size())
+                    return false;
+                if (this.lookup(i) != l.lookup(i))
+                    return false;
+            }
+
+            return true;
+        }
+        else
+            return false;
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
