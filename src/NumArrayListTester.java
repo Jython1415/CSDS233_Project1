01f70b4276baa17df5fa7db40c3e6ee62c0dc994
Joshua Shew <joshua.t.shew@gmail.com>
2022-01-22 14:59:07 -0500

    Tests for add method

--- src/NumArrayListTester.java
+++ src/NumArrayListTester.java
@@ -34,6 +34,24 @@ public class NumArrayListTester {
     }
 
     /**
+     * Compares a NumArrayList to an array and asserts that they are equal using org.junit.Assert
+     * The method uses reflection to access the private field storing the internal array, and compares that to the array directly
+     * @param message the String message to report when the lists are not equal
+     * @param list1 the NumArrayList to be compared
+     * @param values the array to be compared
+     */
+    private static void assertNumArrayListEquals(String message, NumArrayList list1, double[] array) {
+        try {
+            Assert.assertTrue(message,
+                          list1.getClass().getField("internalArray").equals(
+                          array));
+        }
+        catch (NoSuchFieldException e) {
+            Assert.fail("There was an unexpected exception thrown by the unit test");
+        }
+    }
+
+    /**
      * Unit tests for the NumArrayList constructors
      */
     @Test
@@ -62,7 +80,18 @@ public class NumArrayListTester {
      */
     @Test
     public void testAdd() {
-        
+        // 0 elements
+        NumArrayList list1 = new NumArrayList();
+        assertNumArrayListEquals("list1 was not empty when it should have been", list1, new double[] {});
+
+        // 1 element
+        list1.add(1.0);
+        assertNumArrayListEquals("list1 did not have the correct element", list1, new double[] {1.0});
+
+        // 3 elements
+        list1.add(2.0);
+        list1.add(3.0);
+        assertNumArrayListEquals("list1 did not have the correct elements", list1, new double[] {1.0, 2.0, 3.0});
     }
 
     /**
