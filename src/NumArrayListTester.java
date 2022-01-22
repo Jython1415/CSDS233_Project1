7749bfc8d5091ba07d615ef89ffb1808ac9d0559
Joshua Shew <joshua.t.shew@gmail.com>
2022-01-22 13:41:52 -0500

    Set up tester class

--- src/NumArrayListTester.java
+++ src/NumArrayListTester.java
@@ -2,5 +2,92 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class NumArrayListTester {
+    /**
+     * 
+     */
+    @Test
+    public void testNumArrayList() {
 
+    }
+    
+    /**
+     * 
+     */
+    @Test
+    public void testSize() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testCapacity() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testAdd() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public  void testInsert() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public  void testRemove() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testContains() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testLookup() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testEquals() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testRemoveDuplicates() {
+
+    }
+
+    /**
+     * 
+     */
+    @Test
+    public void testToString() {
+
+
+    }
 }
